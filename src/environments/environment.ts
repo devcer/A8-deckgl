252dd4ab244e9af22f9f43a1ba250cffd9c22efc
santoshv <santoshv@PRINHYLTPAP1578.local>
2019-11-27 13:57:00 +0530

    First Initial Commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapboxToken: 'pk.eyJ1Ijoidmlzd2FuYXRoYW1zYW50b3NoIiwiYSI6ImNrMGt4dHBlNTBrOWYzbnRrNXBodzhnZ2gifQ.6n2HTrCLjfNahR0sY8GGVg'
 };
 
 /*
